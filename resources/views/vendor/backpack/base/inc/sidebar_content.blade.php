a88bbd694f49baea84d905eebb2b31b8b00170c8
SatpamKampus <gregorius.yoga16@gmail.com>
2023-04-25 18:20:15 +0700

    Add Category and Sub Category (unfixed)

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -11,3 +11,5 @@
     </ul>
 </li>
 @endif
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-question'></i> Categories</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('sub-category') }}'><i class='nav-icon la la-question'></i> Sub categories</a></li>
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -17,4 +17,6 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::crud('location', 'LocationCrudController');
+    Route::crud('category', 'CategoryCrudController');
+    Route::crud('sub-category', 'SubCategoryCrudController');
 }); // this should be the absolute last line of this file
